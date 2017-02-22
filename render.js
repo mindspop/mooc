2c1488dbc909cc55eb810c723db1fef807b5d1a8
朱超强 <mindspop@gmail.com>
2017-02-22 16:28:35 +0800

    fix: render base path

--- render.js
+++ render.js
@@ -1,11 +1,14 @@
+const path = require('path')
+
 const gulp = require('gulp')
 const aglio = require('gulp-aglio')
 
 const options = require('./config').get('renderOptions')
+const cwd = process.cwd()
 
 function render(pattern) {
   return function renderAPI(done) {
-    return gulp.src(pattern)
+    return gulp.src(pattern, { base: path.resolve(cwd, 'api') })
       .pipe(aglio(options))
       .pipe(gulp.dest(options.dest))
   }
--- watch.js
+++ watch.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const gulp = require('gulp')
 const aglio = require('gulp-aglio')
 const gutil = require('gulp-util')
@@ -5,15 +7,17 @@ const gutil = require('gulp-util')
 const options = require('./config').get('base')
 const render = require('./render')
 
+const cwd = process.cwd()
+
 module.exports = function watch(done) {
   gulp.watch(options.apiFiles)
-  .on('change', function(path, stats) {
-    gutil.log(`${path} changed`)
-    render(path)()
+  .on('change', function(p, stats) {
+    gutil.log(`${p} changed`)
+    render(path.resolve(cwd, p))()
   })
-  .on('add', function(path, stats) {
-    gutil.log(`${path} added`)
-    render(path)()
+  .on('add', function(p, stats) {
+    gutil.log(`${p} added`)
+    render(path.resolve(cwd, p))()
   })
 
   done()
